9a888270bffd656f334d9d1c6b17b4a25e858686
codeforsure <haitovarun@gmail.com>
2017-02-05 19:39:49 +0530

    Add files via upload

---
+++ MyServlet.java
@@ -0,0 +1,21 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+public class MyServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try  {
+            out.println("<h2>welcome to my first servelet</h2>");
+        }
+        finally{
+            out.close();
+        }
+    }
+}
